a80dcba876f9040e68a5ac850d785a1a83a5a328
bob <example@maeail.com>
2022-12-31 11:56:45 +0200

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,74 +1,85 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      variableWidth: true,
-      prevArrow: '<button type="button" class="slick-prev"> <img src="icons/icon_arrow_left.png">  </button>',
-      nextArrow: '<button type="button" class="slick-next"> <img src="icons/icon_arrow_right.png"> </button>',
-      responsive: [
-        {
-          breakpoint: 991,
-          settings: {
-            arrows: false
-          }
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow:   2,
-            slidesToScroll: 2,
-            arrows: false    
-          }
-        },
-        {
-          breakpoint: 575,
-          settings: {
-            arrows: false
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
+    
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-       $(this)
-      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
-    
-    
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-      function toggleSlide(item) {
+    function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
+            })
         });
-    }
+    };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modal
 
-    // modal
-
-    $('[data-modal=consultation]').on('click',function(){
-      $('.overlay, #consultation').fadeIn('slow');
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
     });
     $('.modal__close').on('click', function() {
-      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
+
     $('.button_mini').each(function(i) {
-      $(this).on('click', function(){
-        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-        $('.overlay, #order').fadeIn('slow');
-      })
-    })
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
 
-  });
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+});
 
